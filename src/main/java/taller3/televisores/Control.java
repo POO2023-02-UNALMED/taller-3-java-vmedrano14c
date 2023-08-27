4979a5671e168123ffa4595c2a30d785b43a3481
vmedrano14c <141966048+vmedrano14c@users.noreply.github.com>
2023-08-27 18:46:43 -0500

    prueba
    intento numero mil

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -3,9 +3,8 @@ package taller3.televisores;
 public class Control {
 	private TV tv;
 	
-	public void enlazar(TV tv) {
+	public Control (TV tv){
 		this.tv = tv;
-		tv.enlazarControl(this);
 	}
 	
 	public void turnOn() {
