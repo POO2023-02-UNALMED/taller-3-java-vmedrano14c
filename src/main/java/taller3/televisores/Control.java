6a054f0d775f74bd0057d74083b732eba55dd531
vmedrano14c <141966048+vmedrano14c@users.noreply.github.com>
2023-08-26 19:16:53 -0500

    prueba 4
    No entiendo nada

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,50 +1,66 @@
 package taller3.televisores;
 
 public class Control {
-private TV tv;
-private Control control;
+	private TV tv;
 	
-	public void enlazar(TV tv, Control control) {
+	public void enlazar(TV tv) {
 		this.tv = tv;
-		tv.setControl(control);
+		tv.enlazarControl(this);
 	}
-	public void setTV(TV tv) {
-		this.tv = tv;
+	
+	public void turnOn() {
+		if (tv != null) {
+			this.tv.turnOn();
+		}
+	}
+	
+	public void turnOff() {
+		if (tv != null) {
+			this.tv.turnOff();
+		}
+	}
+	
+	public void canalUp() {
+		if (tv != null) {
+			this.tv.canalUp();
+		}	
+	}
+	
+	public void canalDown() {
+		if (tv != null) {
+			this.tv.canalDown();
+		}
+	}
+	
+	public void volumenUp() {
+		if (tv != null) {
+			this.tv.volumenUp();
+		}
+	}
+	
+	public void volumenDown() {
+		if (tv != null) {
+			this.tv.volumenDown();
+		}
+	}
+	
+	public void setCanal(int canal) {
+		if (tv != null) {
+			this.tv.setCanal(canal);
+		}
+	}
+	
+	public void setVolumen(int volumen) {
+		if (tv != null) {
+			this.tv.setVolumen(volumen);
+		}
 	}
 	
 	public TV getTv() {
 		return tv;
 	}
 	
-	public void turnOn() {
-		this.tv.turnOn();
+	public void setTv(TV tv) {
+		this.tv = tv;
 	}
-	
-	public void turnOff() {
-		this.tv.turnOff();
-	}
-	
-	public void canalUp() {
-		this.tv.canalUp();	
-	}
-	
-	public void canalDown() {
-		this.tv.canalDown();
-	}
-	
-	public void volumenUp() {
-		this.tv.volumenUp();
-	}
-	
-	public void volumenDown() {
-		this.tv.volumenDown();
-	}
-	
-	public void setCanal(int canal) {
-		this.tv.setCanal(canal);
-	}
-	
-	public void setVolumen(int volumen) {
-		this.tv.setVolumen(volumen);
-	}	
 }
